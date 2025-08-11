e1407d636f18475046dc166cd45ab9a7600a24ea
flowr1x <bgpfnic@protonmail.com>
2025-08-11 18:44:14 +0300

    add darkmode

--- src/components/MainHeader.vue
+++ src/components/MainHeader.vue
@@ -1,10 +1,9 @@
 <script lang="ts" setup>
-import { useDark } from '@vueuse/core'
-// const isDark = useDark()
+import ToogleDark from './UI/ToogleDark.vue';
 </script>
 <template>
   <header class="header">
-    <div class="container">
+    <div class="container header__container">
       <div class="header__logo">
         <svg width="91" height="14" viewBox="0 0 91 14" fill="none" xmlns="http://www.w3.org/2000/svg">
           <path
@@ -13,33 +12,8 @@ import { useDark } from '@vueuse/core'
           />
         </svg>
       </div>
-      <div>
-        <button class="btn-toggle-theme">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke-width="1"
-            stroke="white"
-            class="btn-toggle-theme__icon btn-toggle-theme__sun"
-          >
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              d="M12 3v2.25m6.364.386-1.591 1.591M21 12h-2.25m-.386 6.364-1.591-1.591M12 18.75V21m-4.773-4.227-1.591 1.591M5.25 12H3m4.227-4.773L5.636 5.636M15.75 12a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0Z"
-              class="btn-toggle-theme__icon btn-toggle-theme__moon"
-            />
-          </svg>
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="size-6">
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="1"
-              stroke="white"
-              d="M21.752 15.002A9.72 9.72 0 0 1 18 15.75c-5.385 0-9.75-4.365-9.75-9.75 0-1.33.266-2.597.748-3.752A9.753 9.753 0 0 0 3 11.25C3 16.635 7.365 21 12.75 21a9.753 9.753 0 0 0 9.002-5.998Z"
-            />
-          </svg>
-        </button>
+      <div class="header__block">
+        <ToogleDark />
       </div>
     </div>
   </header>
@@ -49,19 +23,14 @@ import { useDark } from '@vueuse/core'
 .header {
   padding: 15px 0;
   background-color: var(--header-bg);
+  &__container {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+  &__block {
+    display: flex;
+  }
 }
-.btn-toggle-theme {
-  height: 24px;
-  width: 24px;
-  background-color: var(--gray);
-  border-radius: 4px;
-  padding: 4px;
 
-  & svg path {
-    fill: var(--white);
-    stroke: var(--white);
-  }
-  &.dark {
-  }
-}
 </style>
---
+++ src/components/UI/MainIcon.vue
@@ -0,0 +1,33 @@
+
+<script setup lang="ts">
+import { computed, defineProps } from 'vue'
+const icons = import.meta.glob('@/components/icons/*.vue', { eager: true })
+
+type Props = {
+  name: string
+}
+
+const props = defineProps<Props>();
+console.log(icons)
+const iconComponent = computed(() => {
+  const fileName = `/src/components/icons/Icon${props.name}.vue`
+  console.log(fileName)
+  return icons[fileName]?.default || null
+})
+console.log(iconComponent.value)
+</script>
+
+<template>
+  <component
+    :is="iconComponent"
+    v-bind="$attrs"
+    class="icon"
+  />
+</template>
+
+<style scoped>
+.icon {
+  display: inline-block;
+  vertical-align: middle;
+}
+</style>
